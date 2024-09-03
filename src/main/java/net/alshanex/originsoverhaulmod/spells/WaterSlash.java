63e91976f9e3c86f41e73a898cb498a829479d00
Alshanex <discordpermacc02@gmail.com>
2024-09-03 18:23:09 +0200

    Fixed bugs

--- src/main/java/net/alshanex/originsoverhaulmod/spells/WaterSlash.java
+++ src/main/java/net/alshanex/originsoverhaulmod/spells/WaterSlash.java
@@ -6,7 +6,7 @@ import io.redspace.ironsspellbooks.api.magic.MagicData;
 import io.redspace.ironsspellbooks.api.registry.SchoolRegistry;
 import io.redspace.ironsspellbooks.api.spells.*;
 import io.redspace.ironsspellbooks.api.util.Utils;
-import io.redspace.ironsspellbooks.capabilities.magic.CastTargetingData;
+import io.redspace.ironsspellbooks.capabilities.magic.TargetEntityCastData;
 import io.redspace.ironsspellbooks.damage.SpellDamageSource;
 import net.alshanex.originsoverhaulmod.entity.custom.WaterSlashEntity;
 import net.minecraft.network.chat.Component;
@@ -75,7 +75,7 @@ public class WaterSlash extends AbstractSpell{
 
     @Override
     public void onCast(Level world, int spellLevel, LivingEntity entity, CastSource castSource, MagicData playerMagicData) {
-        if (playerMagicData.getAdditionalCastData() instanceof CastTargetingData targetData) {
+        if (playerMagicData.getAdditionalCastData() instanceof TargetEntityCastData targetData) {
             var targetEntity = targetData.getTarget((ServerLevel) world);
             if (targetEntity != null) {
                 WaterSlashEntity slash = new WaterSlashEntity(world, entity, targetEntity);
